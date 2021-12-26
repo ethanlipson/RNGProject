22f1ab32cdca79005f2555fa7e3688742bd5de23
Ian Graham Martinez <Ian.graham28@gmail.com>
2021-12-26 13:52:31 -0500

    basics of authentication keys

--- casino/common.php
+++ casino/common.php
@@ -1,24 +1,29 @@
 <?php
-//boilerplate goes here...
+//boilerplate goes here.
 
 function customError($errno, $errstr, $file, $line, $context) {
-	echo "<b>Error in $file, line $line :</b>\n[$errno] $errstr\n\n$context";
+	echo "<b>Error in $file, line $line :</b>\n[$errno] $errstr\n\n$context\n\n\n";
 }
 set_error_handler("customError");
 
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
-$db = parse_url(getenv("DATABASE_URL"));
 
 $pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
+	"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	$db["host"],
+	$db["port"],
+	$db["user"],
+	$db["pass"],
+	ltrim($db["path"], "/")
 ));
 
+$user = new stdClass();
 
+if(isset($_COOKIE['authentication'])) {
+	$statement = $pdo->prepare("SELECT * FROM cookies where cookie = :cookie;", $_COOKIE['authentication']);
+	$row = $statement->fetch();
+	$user->username = $row->username;
+}
 
 ?>
--- casino/index.php
+++ casino/index.php
@@ -1,6 +1,4 @@
 <?php
 include 'common.php';
-echo $db;
-echo $conn;
 
 ?>
